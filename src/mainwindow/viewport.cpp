5ee072fc4143ee6d3dbf23e40410c2cb84563717
pascal <pascal.bies@k-lens.de>
2018-11-17 18:53:08 +0100

    viewport pan

--- src/mainwindow/viewport.cpp
+++ src/mainwindow/viewport.cpp
@@ -3,15 +3,34 @@
 #include <glog/logging.h>
 #include <QPainter>
 #include <QTimer>
+#include <QMouseEvent>
 
 #include "renderers/viewportrenderer.h"
+
+namespace
+{
+arma::vec2 point2vec(const QPoint& p)
+{
+  return arma::vec2 {
+    static_cast<double>(p.x()),
+    static_cast<double>(p.y()),
+  };
+}
+
+void set_cursor_position(QWidget& widget, const arma::vec2& pos)
+{
+  auto cursor = widget.cursor();
+  cursor.setPos(widget.mapToGlobal(QPoint(pos(0), pos(1))));
+  widget.setCursor(cursor);
+}
+
+}  // namespace
 
 namespace omm
 {
 
-Viewport::Viewport(Scene& scene, QWidget* parent)
-  : QWidget(parent)
-  , m_scene(scene)
+Viewport::Viewport(Scene& scene)
+  : m_scene(scene)
   , m_timer(std::make_unique<QTimer>())
 {
   setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
@@ -27,12 +46,67 @@ void Viewport::paintEvent(QPaintEvent* event)
   QPainter painter(this);
   painter.fillRect(rect(), Qt::gray);
 
+  const auto width = static_cast<double>(this->width());
+  const auto height = static_cast<double>(this->height());
   const BoundingBox render_box({ { 0, 0 },
-                                 { double(width()), double(height()) } });
-  ViewportRenderer(painter, render_box).render(m_scene);
+                                 { width, height } });
+  ViewportRenderer renderer(painter, render_box);
+  auto viewport_transformation = m_viewport_transformation;
+  viewport_transformation.set_translation(  viewport_transformation.translation()
+                                          + arma::vec2{ width/2, height/2} );
+  renderer.set_base_transformation(viewport_transformation);
+  renderer.render(m_scene);
 }
 
+void Viewport::mousePressEvent(QMouseEvent* event)
 {
+  MousePanArea::init();
+
+  if (event->modifiers() & Qt::AltModifier) {
+    event->accept();
+  } else {
+    QWidget::mousePressEvent(event);
+  }
+}
+
+void Viewport::mouseMoveEvent(QMouseEvent* event)
+{
+  if (event->modifiers() & Qt::AltModifier ) {
+    const auto delta = MousePanArea::delta(point2vec(event->pos()));
+    m_viewport_transformation.translate(delta);
+    event->accept();
+  } else {
+    QWidget::mouseMoveEvent(event);
+  }
+}
+
+void MousePanArea::init()
+{
+  m_last_position = point2vec(QWidget::mapFromGlobal(cursor().pos()));
+}
+
+arma::vec2 MousePanArea::delta(arma::vec2 new_position)
+{
+  auto cursor = QWidget::cursor();
+  const decltype(m_last_position) delta = new_position - m_last_position;
+
+  const auto infinity_pan_shift = [](double& coordinate, const double max) {
+    while (coordinate >= max) {
+      coordinate -= max;
+    }
+    while (coordinate <= 0) {
+      coordinate += max;
+    }
+  };
+
+  infinity_pan_shift(new_position(0), width());
+  infinity_pan_shift(new_position(1), height());
+
+  cursor.setPos(QWidget::mapToGlobal(QPoint(new_position(0), new_position(1))));
+  // QWidget::setCursor(cursor);
+  m_last_position = new_position;
+
+  return delta;
 }
 
 }  // namespace omm
--- src/mainwindow/viewport.h
+++ src/mainwindow/viewport.h
@@ -4,22 +4,37 @@
 #include <QWidget>
 #include <QTimer>
 
+#include "geometry/objecttransformation.h"
+
 namespace omm
 {
 
 class Scene;
 
-class Viewport : public QWidget
+class MousePanArea : public QWidget
 {
 public:
-  Viewport(Scene& scene, QWidget* parent = nullptr);
+  void init();
+  arma::vec2 delta(arma::vec2 pos);
+private:
+  arma::vec2 m_last_position;
+};
+
+class Viewport : public MousePanArea
+{
+public:
+  Viewport(Scene& scene);
 
 protected:
   void paintEvent(QPaintEvent* event);
+  void mousePressEvent(QMouseEvent* event) override;
+  void mouseMoveEvent(QMouseEvent* event) override;
 
 private:
   Scene& m_scene;
   std::unique_ptr<QTimer> m_timer;
+  ObjectTransformation m_viewport_transformation;
+  arma::vec2 m_last_position;
 };
 
 }  // namespace omm
