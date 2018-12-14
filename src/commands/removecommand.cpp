7bfc97f213247919238c4faff079e3aa583950fb
pascal <pascal.bies@k-lens.de>
2018-12-15 00:22:20 +0100

    fixed undo-remove-multiple items bug

--- src/commands/removecommand.cpp
+++ src/commands/removecommand.cpp
@@ -10,15 +10,59 @@
 namespace
 {
 
+template<typename iterator, typename pred>
+iterator move_if(iterator begin, iterator end, const pred& predicate)
+{
+  const iterator it = std::find_if(begin, end, predicate);
+  if (it != end) {
+    std::iter_swap(it, std::prev(end));
+    return std::prev(end);
+  } else {
+    return it;
+  }
+}
+
+template<typename T>
+void stupid_topological_sort(std::vector<T>& ts)
+{
+  // Stupid because I guess it's O(n^2). It's possible to do it in O(n).
+  const auto begin_ts = ts.begin();
+  auto end_ts = ts.end();
+
+  while (begin_ts != end_ts) {
+    end_ts = move_if(begin_ts, end_ts, [&begin_ts, &end_ts](const auto& t) {
+      return end_ts == std::find_if(begin_ts, end_ts, [&t](const auto& s) {
+        return t.predecessor == &s.subject.get();
+      });
+    });
+
+    while (true) {
+      const auto current = move_if(begin_ts, end_ts, [&end_ts](const auto& t) {
+        return &end_ts->subject.get() == t.predecessor;
+      });
+      if (current == end_ts) {
+        break;
+      } else {
+        end_ts = current;
+      }
+    }
+  }
+}
+
 template<typename StructureT>
 auto make_contextes( const StructureT& structure,
                      const std::set<typename StructureT::item_type*>& selection )
 {
-  std::vector<typename omm::Contextes<typename StructureT::item_type>::Owning> contextes;
+  using item_type = typename StructureT::item_type;
+  using context_type = typename omm::Contextes<item_type>::Owning;
+  std::vector<context_type> contextes;
   contextes.reserve(selection.size());
   for (auto item : selection) {
     contextes.emplace_back(*item, structure.predecessor(*item));
   }
+
+  stupid_topological_sort(contextes);
+  std::reverse(contextes.begin(), contextes.end());
 
   return contextes;
 }
@@ -50,12 +94,10 @@ template<typename StructureT> void RemoveCommand<StructureT>::redo()
 
 template<typename StructureT> void RemoveCommand<StructureT>::undo()
 {
-  for (auto&& it = m_contextes.rbegin(); it != m_contextes.rend(); ++it) {
-    assert(it->subject.owns());
-    auto& context = *it;
-
+  for (auto&& context : m_contextes) {
+    assert(context.subject.owns());
     // if predecessor is not null, it must had been inserted in the object tree.
-    // assert(context.predecessor == nullptr || !context.predecessor->is_root()); // TODO
+    assert(context.subject.owns());
     m_structure.insert(context);
   }
   // m_structure.selection_changed();  // TODO
