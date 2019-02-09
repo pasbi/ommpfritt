e4590a36875df6e4ab811be44d70dde3f931b859
pascal <pascal.bies@k-lens.de>
2019-02-09 12:33:46 +0100

    fixed tab spacing in `CodeEdit`

--- src/widgets/codeedit.cpp
+++ src/widgets/codeedit.cpp
@@ -13,8 +13,24 @@ CodeEdit::CodeEdit(QWidget* parent)
   std::make_unique<QHBoxLayout>(this).release();
   layout()->addWidget(text_edit.release());
   layout()->setContentsMargins(0, 0, 0, 0);
+
+  // see https://stackoverflow.com/a/54605709/4248972
+
+  static constexpr int tab_width_char = 2;
   m_text_edit->setFont(QFont("Courier", 12));
-  m_text_edit->setTabStopDistance(fontMetrics().width("em"));
+  const auto font_metrics = m_text_edit->fontMetrics();
+
+  static constexpr int big_number = 1000; // arbitrary big number.
+  const QString test_string(" ");
+
+  // compute the size of a char in double-precision
+  const int single_char_width = font_metrics.width(test_string);
+  const int many_char_width = font_metrics.width(test_string.repeated(big_number));
+  const double single_char_width_double = many_char_width / double(big_number);
+
+  // set the tab stop with double-precision
+  m_text_edit->setTabStopDistance(tab_width_char * single_char_width_double);
+
   connect(m_text_edit, &QTextEdit::textChanged, [this]() {
     Q_EMIT this->code_changed(code());
   });
