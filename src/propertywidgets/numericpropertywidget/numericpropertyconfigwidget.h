46e7e7d577c1cfa186bdce32a744851fd1fe7e1a
Pascal Bies <pascal.bies@k-lens.de>
2019-02-13 23:34:44 +0100

    property config has confugurable step; improved min/max configuration

--- src/propertywidgets/numericpropertywidget/numericpropertyconfigwidget.h
+++ src/propertywidgets/numericpropertywidget/numericpropertyconfigwidget.h
@@ -19,10 +19,31 @@ public:
   NumericPropertyConfigWidget(QWidget* parent, Property& property)
     : PropertyConfigWidget<PropertyT>(parent, property)
   {
-    auto min_widget = std::make_unique<NumericEdit<value_type>>([](const value_type&) {});
-    this->form_layout()->addRow(NumericPropertyConfigWidget::tr("min"), min_widget.release());
-    auto max_widget = std::make_unique<NumericEdit<value_type>>([](const value_type&) {});
-    this->form_layout()->addRow(NumericPropertyConfigWidget::tr("max"), max_widget.release());
+    auto& numeric_property = type_cast<PropertyT&>(property);
+    const auto on_min_max_changed = [&numeric_property](const value_type min, const value_type max)
+    {
+      numeric_property.set_range(min, max);
+    };
+    auto [ min_edit, max_edit ] = NumericEdit<value_type>::make_min_max_edits(on_min_max_changed);
+    const auto on_step_changed = [&numeric_property](value_type step) {
+      if (step == 0) {
+
+      }
+      numeric_property.set_step(step);
+    };
+    auto step_edit = std::make_unique<NumericEdit<value_type>>(on_step_changed);
+    if constexpr (std::is_floating_point_v<value_type>) {
+      step_edit->set_lower(std::numeric_limits<value_type>::min());
+    } else {
+      step_edit->set_lower(1);
+    }
+    step_edit->set_value(numeric_property.step());
+    min_edit->set_value(numeric_property.lower());
+    max_edit->set_value(numeric_property.upper());
+
+    this->form_layout()->addRow(NumericPropertyConfigWidget::tr("min"), min_edit.release());
+    this->form_layout()->addRow(NumericPropertyConfigWidget::tr("max"), max_edit.release());
+    this->form_layout()->addRow(NumericPropertyConfigWidget::tr("step"), step_edit.release());
   }
 };
 
--- src/widgets/numericedit.h
+++ src/widgets/numericedit.h
@@ -6,12 +6,10 @@
 #include <cmath>
 #include <QWheelEvent>
 #include <glog/logging.h>
+#include <memory>
 
 namespace detail
 {
-
-constexpr auto inf = "inf";
-constexpr auto neg_inf = "-inf";
 
 // they must be initialized, though they should never be used.
 template<typename T> T highest_possible_value = T();
@@ -21,7 +19,7 @@ template<typename T> T lowest_possible_value = T();
 template<> constexpr double highest_possible_value<double>
                           = std::numeric_limits<double>::infinity();
 template<> constexpr double lowest_possible_value<double>
-                          = std::numeric_limits<double>::infinity();
+                          = -std::numeric_limits<double>::infinity();
 template<> constexpr int highest_possible_value<int> = std::numeric_limits<int>::max();
 template<> constexpr int lowest_possible_value<int> = std::numeric_limits<int>::lowest();
 
@@ -34,6 +32,10 @@ template<typename ValueType>
 class NumericEdit : public QLineEdit
 {
 public:
+
+  static constexpr auto inf = "inf";
+  static constexpr auto neg_inf = "-inf";
+
   using value_type = ValueType;
   using on_value_changed_t = std::function<void(value_type)>;
   NumericEdit(const on_value_changed_t& on_value_changed, QWidget* parent = nullptr)
@@ -50,11 +52,34 @@ public:
     set_text(invalid_value);
   }
 
-  void set_range(value_type min, value_type max)
+  void set_range(const value_type min, const value_type max)
   {
-    assert(min < max);
-    m_min = min;
-    m_max = max;
+    if (min > max) {
+      LOG(ERROR) << "min is greater than max: " << min << " > " << max;
+      m_min = min;
+      m_max = min;
+    } else {
+      m_min = min;
+      m_max = max;
+    }
+  }
+
+  void set_lower(const value_type min)
+  {
+    if (min > m_max) {
+      LOG(ERROR) << "min is greater than max: " << min << " > " << m_max;
+    } else {
+      m_min = min;
+    }
+  }
+
+  void set_upper(const value_type max)
+  {
+    if (m_min > max) {
+      LOG(ERROR) << "max is greater than min: " << m_min << " > " << max;
+    } else {
+      m_max = max;
+    }
   }
 
   void set_step(value_type step)
@@ -164,9 +189,9 @@ private:
 
   value_type parse(const std::string& text) const
   {
-    if (text == detail::inf) {
+    if (text == inf) {
       return detail::highest_possible_value<value_type>;
-    } else if (text == detail::neg_inf) {
+    } else if (text == neg_inf) {
       return detail::lowest_possible_value<value_type>;
     } else {
       std::istringstream sstream(text);
@@ -186,12 +211,46 @@ private:
     }
   }
 
-  on_value_changed_t m_on_value_changed;
+  const on_value_changed_t m_on_value_changed;
   void set_text(const value_type& value)
   {
     setText(QString("%1").arg(value_type(m_multiplier * value)));
   }
   static constexpr value_type invalid_value = 0;
+
+public:
+  using on_min_max_changed_t = std::function<void(const ValueType& min, const ValueType& max)>;
+  static auto make_min_max_edits(const on_min_max_changed_t& on_min_max_changed)
+  {
+    enum class MasterBound { Upper, Lower };
+    auto min_edit = std::make_unique<NumericEdit<ValueType>>([](const ValueType&) {});
+    auto max_edit = std::make_unique<NumericEdit<ValueType>>([](const ValueType&) {});
+
+    auto on_bound_changed = [on_min_max_changed, min_edit=min_edit.get(), max_edit=max_edit.get()]
+                            (MasterBound master_bound)
+    {
+      double max = max_edit->value();
+      double min = min_edit->value();
+      if (max < min) {
+        switch (master_bound) {
+        case MasterBound::Upper:
+          min = max;
+          min_edit->set_value(min);
+          break;
+        case MasterBound::Lower:
+          max = min;
+          max_edit->set_value(max);
+          break;
+        }
+      }
+      on_min_max_changed(min, max);
+    };
+    const auto on_lower_changed = std::bind(on_bound_changed, MasterBound::Lower);
+    connect(min_edit.get(), &QLineEdit::editingFinished, on_lower_changed);
+    const auto on_upper_changed = std::bind(on_bound_changed, MasterBound::Upper);
+    connect(max_edit.get(), &QLineEdit::editingFinished, on_upper_changed);
+    return std::pair(std::move(min_edit), std::move(max_edit));
+  }
 };
 
 }  // namespace
