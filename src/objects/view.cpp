99a93c2843ffbd10e5eae3992ac3b5370e47a1ae
Pascal Bies <pascal.bies@k-lens.de>
2019-05-04 12:07:13 +0200

    New  output(bool)-property for View

--- src/objects/view.cpp
+++ src/objects/view.cpp
@@ -7,6 +7,7 @@
 #include "mainwindow/application.h"
 #include "mainwindow/mainwindow.h"
 #include "mainwindow/viewport/viewport.h"
+#include "properties/boolproperty.h"
 
 namespace omm
 {
@@ -20,6 +21,15 @@ View::View(Scene* scene) : Object(scene)
     .set_label(QObject::tr("to viewport").toStdString()).set_category(category);
   add_property<TriggerProperty>(FROM_VIEWPORT_PROPERTY_KEY)
     .set_label(QObject::tr("from viewport").toStdString()).set_category(category);
+  add_property<BoolProperty>(OUTPUT_VIEW_PROPERTY_KEY, false)
+      .set_label(QObject::tr("output").toStdString()).set_category(category);
+}
+
+View::View(const View &other) : Object(other)
+{
+  if (property(OUTPUT_VIEW_PROPERTY_KEY)->value<bool>()) {
+    make_output_unique();
+  }
 }
 
 std::string View::type() const { return TYPE; }
@@ -31,6 +41,16 @@ void View::to_viewport()
 {
   auto& viewport = Application::instance().main_window()->viewport();
   viewport.set_transformation(transformation());
+}
+
+void View::make_output_unique()
+{
+  const auto views = type_cast<View*>(scene()->object_tree.items());
+  for (View* view : views) {
+    auto& property = *view->property(OUTPUT_VIEW_PROPERTY_KEY);
+    Property::NotificationBlocker blocker(property);
+    property.set(view == this);
+  }
 }
 
 void View::draw_handles(AbstractRenderer& renderer) const
@@ -49,6 +69,7 @@ void View::on_property_value_changed(Property& property)
 {
   if (&property == this->property(TO_VIEWPORT_PROPERTY_KEY)) { to_viewport(); }
   if (&property == this->property(FROM_VIEWPORT_PROPERTY_KEY)) { from_viewport(); }
+  if (&property == this->property(OUTPUT_VIEW_PROPERTY_KEY)) { make_output_unique(); }
 }
 
 void View::from_viewport()
--- src/objects/view.h
+++ src/objects/view.h
@@ -9,6 +9,7 @@ class View : public Object
 {
 public:
   explicit View(Scene* scene);
+  explicit View(const View& other);
   void draw_handles(AbstractRenderer& renderer) const override;
   BoundingBox bounding_box() const override;
   std::string type() const override;
@@ -16,13 +17,14 @@ public:
   static constexpr auto SIZE_PROPERTY_KEY = "size";
   static constexpr auto TO_VIEWPORT_PROPERTY_KEY = "to-viewport";
   static constexpr auto FROM_VIEWPORT_PROPERTY_KEY = "from-viewport";
+  static constexpr auto OUTPUT_VIEW_PROPERTY_KEY = "output";
   static constexpr auto TYPE = QT_TRANSLATE_NOOP("any-context", "View");
   void on_property_value_changed(Property& property) override;
   Flag flags() const override;
-
 private:
   void from_viewport();
   void to_viewport();
+  void make_output_unique();
 };
 
 }  // namespace omm
