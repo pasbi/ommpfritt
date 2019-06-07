21d3b5bd1f54338e6db730a32d934b6fe5e035b7
Pascal Bies <pascal.bies@k-lens.de>
2019-06-07 11:42:13 +0200

    scaling of bounding box somehow work, but not yet nicely.

--- src/tools/handles/boundingboxhandle.h
+++ src/tools/handles/boundingboxhandle.h
@@ -21,12 +21,12 @@ public:
   bool contains_global(const Vec2f& point) const override
   {
     static constexpr double eps = 4;
-    const BoundingBox bounding_box = static_cast<const ToolT&>(tool).bounding_box();
-    const bool left_fringe = std::abs(bounding_box.left() - point.x) < eps;
-    const bool right_fringe = std::abs(bounding_box.right() - point.x) < eps;
-    const bool top_fringe = std::abs(bounding_box.top() - point.y) < eps;
-    const bool bottom_fringe = std::abs(bounding_box.bottom() - point.y) < eps;
-    return left_fringe || right_fringe || top_fringe || bottom_fringe;
+    m_bounding_box = static_cast<const ToolT&>(tool).bounding_box();
+    m_left_fringe = std::abs(m_bounding_box.left() - point.x) < eps;
+    m_right_fringe = std::abs(m_bounding_box.right() - point.x) < eps;
+    m_top_fringe = std::abs(m_bounding_box.top() - point.y) < eps;
+    m_bottom_fringe = std::abs(m_bounding_box.bottom() - point.y) < eps;
+    return m_left_fringe || m_right_fringe || m_top_fringe || m_bottom_fringe;
   }
 
   bool mouse_move(const Vec2f& delta, const Vec2f& pos, const QMouseEvent& e) override
@@ -38,20 +38,25 @@ public:
 
     const auto ti = transformation().inverted();
     const auto global_pos = ti.apply_to_position(pos);
-    const auto origin = ti.apply_to_position(press_pos());
-    const auto delta_ = global_pos - origin;
-    double s = global_pos.euclidean_norm() / (global_pos - delta_).euclidean_norm();
-    if (tool.integer_transformation()) {
-      static constexpr auto step = 0.1;
-      if (std::abs(s) > step) { // s must never be zero.
-        s = step * static_cast<int>(s / step);
-      }
-    }
-    if (constexpr auto eps = 10e-10; std::abs(s) < eps) { s = std::copysign(eps, s); }
+    const auto bounding_box = ti.apply(m_bounding_box);
+    const auto center = ti.apply_to_position(Vec2f::o());
 
-    const auto t = omm::ObjectTransformation().scaled(Vec2f(s, s));
+    const double f = 2;
+    Vec2f s(1.0, 1.0);
+    if (m_left_fringe) {
+      s.x += f * (global_pos.x - bounding_box.left()) / (bounding_box.left() - center.x);
+    } else if (m_right_fringe) {
+      s.x -= f * (global_pos.x - bounding_box.right()) / (bounding_box.right() - center.x);
+    }
+    s.x = 1.0 / s.x;
+    if (m_top_fringe) {
+      s.y -= f * (global_pos.y - bounding_box.top()) / (global_pos.y - center.y);
+    } else if (m_bottom_fringe) {
+      s.y += f * (global_pos.y - bounding_box.bottom()) / (global_pos.y - center.y);
+    }
+    const auto t = omm::ObjectTransformation().scaled(s);
     static_cast<ToolT&>(tool).transform_objects_absolute(t, true);
-    static_cast<ToolT&>(tool).tool_info = QString("%1").arg(s).toStdString();
+    static_cast<ToolT&>(tool).tool_info = QString("%1, %2").arg(s.x).arg(s.y).toStdString();
     return true;
   }
 
@@ -61,6 +66,11 @@ public:
     renderer.set_style(current_style());
     renderer.painter->drawRect(bounding_box);
   }
+  mutable BoundingBox m_bounding_box;
+  mutable bool m_left_fringe = false;
+  mutable bool m_right_fringe = false;
+  mutable bool m_top_fringe = false;
+  mutable bool m_bottom_fringe = false;
 
 };
 
