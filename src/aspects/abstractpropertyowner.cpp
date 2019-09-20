edded7bad41fda627c72ed98fca60b04f4c440ba
Pascal Bies <pascal.bies@k-lens.de>
2019-09-21 01:02:29 +0200

    split {abstract,}propertyowner.{cpp,h} files

---
+++ src/aspects/abstractpropertyowner.cpp
@@ -0,0 +1,180 @@
+#include "aspects/abstractpropertyowner.h"
+
+#include <QObject>
+
+#include "external/json.hpp"
+#include "serializers/abstractserializer.h"
+#include "properties/referenceproperty.h"
+#include "scene/scene.h"
+#include "scene/messagebox.h"
+
+namespace
+{
+  constexpr auto PROPERTIES_POINTER = "properties";
+  constexpr auto PROPERTY_TYPE_POINTER = "type";
+  constexpr auto PROPERTY_KEY_POINTER = "key";
+  constexpr auto ID_POINTER = "id";
+}  // namespace
+
+namespace omm
+{
+
+const std::string AbstractPropertyOwner::NAME_PROPERTY_KEY = "name";
+
+const OrderedMap<std::string, Property>& AbstractPropertyOwner::properties() const
+{
+  return m_properties;
+}
+
+AbstractPropertyOwner::AbstractPropertyOwner(Scene *scene)
+  : m_scene(scene)
+{
+}
+
+AbstractPropertyOwner::AbstractPropertyOwner(const AbstractPropertyOwner &other)
+  : m_scene(other.m_scene)
+{
+  for (auto&& key : other.m_properties.keys()) {
+    add_property(key, other.m_properties.at(key)->clone());
+  }
+}
+
+AbstractPropertyOwner::~AbstractPropertyOwner()
+{
+  for (ReferenceProperty* ref_prop : m_referees) {
+    QSignalBlocker blocker(ref_prop);
+    ref_prop->set(nullptr);
+  }
+}
+
+Property *AbstractPropertyOwner::property(const std::string& key) const
+{
+  if (has_property(key)) {
+    return m_properties.at(key).get();
+  } else {
+    return nullptr;
+  }
+}
+
+bool AbstractPropertyOwner::has_property(const std::string& key) const
+{
+  return m_properties.contains(key);
+}
+
+void AbstractPropertyOwner::serialize(AbstractSerializer& serializer, const Pointer& root) const
+{
+  Serializable::serialize(serializer, root);
+  const auto id_pointer = make_pointer(root, ID_POINTER);
+  serializer.set_value(this, id_pointer);
+
+  const auto properties_pointer = make_pointer(root, PROPERTIES_POINTER);
+  serializer.start_array(m_properties.size(), properties_pointer);
+  for (size_t i = 0; i < m_properties.size(); ++i) {
+    const auto property_key = m_properties.keys().at(i);
+    const auto property_pointer = make_pointer(properties_pointer, i);
+    const auto& property = *this->property(property_key);
+    serializer.set_value(property_key, make_pointer(property_pointer, PROPERTY_KEY_POINTER));
+    serializer.set_value(property.type(), make_pointer(property_pointer, PROPERTY_TYPE_POINTER));
+    property.serialize(serializer, property_pointer);
+  }
+  serializer.end_array();
+}
+
+void AbstractPropertyOwner::deserialize(AbstractDeserializer& deserializer, const Pointer& root)
+{
+  Serializable::deserialize(deserializer, root);
+
+  const auto id_pointer = make_pointer(root, ID_POINTER);
+  m_id = deserializer.get_size_t(id_pointer);
+  deserializer.register_reference(m_id, *this);
+
+  const auto properties_pointer = make_pointer(root, PROPERTIES_POINTER);
+  size_t n_properties = deserializer.array_size(properties_pointer);
+  for (size_t i = 0; i < n_properties; ++i) {
+    const auto property_pointer = make_pointer(properties_pointer, i);
+
+    const auto property_key =
+      deserializer.get_string(make_pointer(property_pointer, PROPERTY_KEY_POINTER));
+    const auto property_type =
+      deserializer.get_string(make_pointer(property_pointer, PROPERTY_TYPE_POINTER));
+
+    if (properties().contains(property_key)) {
+      assert(property_type == property(property_key)->type());
+    } else {
+      try {
+        add_property(property_key, Property::make(property_type));
+      } catch (const std::out_of_range&) {
+        const auto msg = "Failed to retrieve property type '" + property_type + "'.";
+        LERROR << msg;
+        throw AbstractDeserializer::DeserializeError(msg);
+      }
+    }
+
+    property(property_key)->deserialize(deserializer, property_pointer);
+  }
+}
+
+Property
+&AbstractPropertyOwner::add_property(const std::string &key, std::unique_ptr<Property> property)
+{
+  Property& ref = *property;
+  assert(!m_properties.contains(key));
+  m_properties.insert(key, std::move(property));
+  connect(&ref, SIGNAL(value_changed(Property*)),
+          this, SLOT(on_property_value_changed(Property*)));
+  connect(&ref, &Property::value_changed, [this, key](Property* property) {
+    assert(property != nullptr);
+    if (Scene* scene = this->scene(); scene != nullptr) {
+      Q_EMIT scene->message_box().property_value_changed(*this, key, *property);
+    }
+  });
+  return ref;
+}
+
+std::string AbstractPropertyOwner::name() const
+{
+  return property(NAME_PROPERTY_KEY)->value<std::string>();
+}
+
+std::unique_ptr<Property> AbstractPropertyOwner::extract_property(const std::string& key)
+{
+  auto property = m_properties.extract(key);
+  disconnect(property.get(), &Property::value_changed, this, nullptr);
+  return property;
+}
+
+std::ostream& operator<<(std::ostream& ostream, const AbstractPropertyOwner* apo)
+{
+  if (apo == nullptr) {
+    ostream << "AbstractPropertyOwner[nullptr]";
+  } else {
+    const auto kind = static_cast<std::underlying_type_t<decltype(apo->kind())>>(apo->kind());
+    ostream << "AbstractPropertyOwner[" << kind << "]";
+  }
+  return ostream;
+}
+
+void AbstractPropertyOwner::copy_properties(AbstractPropertyOwner& target) const
+{
+  const auto keys = [](const AbstractPropertyOwner& o) {
+    return ::transform<std::string, std::set>(o.properties().keys(), ::identity);
+  };
+
+  for (const auto& key : ::intersect(keys(target), keys(*this))) {
+    const auto& p = *property(key);
+    auto& other_property = *target.property(key);
+    if (other_property.is_compatible(p)) {
+      other_property.set(p.variant_value());
+    }
+  }
+}
+
+std::size_t AbstractPropertyOwner::id() const
+{
+  if (m_id == 0) {
+    m_id = std::hash<const void*>()(this);
+  }
+  return m_id;
+}
+
+}  // namespace omm
--- src/aspects/propertyowner.h
+++ src/aspects/propertyowner.h
@@ -1,107 +1,18 @@
 #pragma once
 
-#include <cassert>
-#include <map>
-#include <memory>
-#include <typeinfo>
-#include <variant>
-
-#include "properties/property.h"
-#include "orderedmap.h"
-#include "external/json_fwd.hpp"
-#include "aspects/serializable.h"
-#include "properties/typedproperty.h"
-#include "properties/stringproperty.h"
-#include "common.h"
-#include <Qt>
-#include "aspects/autoconnectiondeleter.h"
+#include "aspects/abstractpropertyowner.h"
+#include "aspects/animatablepropertyowner.h"
 
 namespace omm
 {
-
-class AbstractPropertyOwner
-    : public QObject, public virtual Serializable, public AutoConnectionDeleter
-{
-  Q_OBJECT
-public:
-  enum class Kind { None = 0x0,
-                    Tag = 0x1, Style = 0x2, Object = 0x4, Tool = 0x8,
-                    Item = Tag | Style | Object, All = Item | Tool };
-
-  enum class Flag { None = 0x0,
-                    Convertable = 0x1, HasScript = 0x2, IsPathLike = 0x4, IsView = 0x8 };
-
-  static constexpr auto TYPE = QT_TRANSLATE_NOOP("PropertyOwner", "AbstractPropertyOwner");
-  explicit AbstractPropertyOwner(Scene* scene);
-  AbstractPropertyOwner(const AbstractPropertyOwner& other);
-  ~AbstractPropertyOwner() override;
-  Property* property(const std::string& key) const;
-  bool has_property(const std::string& key) const;
-  template<typename ValueT> bool has_property(const std::string& key) const
-  {
-    if (has_property(key)) {
-      const auto variant = property(key)->variant_value();
-      return std::get_if<ValueT>(&variant) != nullptr;
-    } else {
-      return false;
-    }
-  }
-
-  virtual Flag flags() const = 0;
-
-  const OrderedMap<std::string, Property>& properties() const;
-
-  void serialize(AbstractSerializer& serializer, const Pointer& root) const override;
-  void deserialize(AbstractDeserializer& deserializer, const Pointer& root) override;
-  virtual std::string name() const;
-
-  virtual Kind kind() const = 0;
-  static const std::string NAME_PROPERTY_KEY;
-
-  Property& add_property(const std::string& key, std::unique_ptr<Property> property);
-
-  template<typename PropertyT, typename... Args>
-  PropertyT& create_property(const std::string& key, Args&&... args)
-  {
-    static_assert(std::is_base_of<Property, PropertyT>::value);
-    auto property = std::make_unique<PropertyT>(std::forward<Args>(args)...);
-    PropertyT& ref = *property;
-    add_property(key, std::move(property));
-    return ref;
-  }
-
-  std::unique_ptr<Property> extract_property(const std::string& key);
-  void copy_properties(AbstractPropertyOwner& target) const;
-
-  std::size_t id() const;
-  Scene* scene() const { return m_scene; }
-
-protected Q_SLOTS:
-  virtual void on_property_value_changed(Property* property) { Q_UNUSED(property); }
-
-private:
-  OrderedMap<std::string, Property> m_properties;
-  Scene* m_scene = nullptr;
-
-  /**
-   * @brief id_proposal if not 0 then this id shall be used for (de)serialization issues.
-   *  if 0 then the proposal shall be ignored and a new id shall be generated instead.
-   */
-  mutable std::size_t m_id = 0;
-
-public:
-  // A set of ReferenceProperties which reference `this`.
-  std::set<ReferenceProperty*> m_referees;
-
-};
 
 template<AbstractPropertyOwner::Kind kind_> class PropertyOwner : public AbstractPropertyOwner
 {
 public:
   using AbstractPropertyOwner::AbstractPropertyOwner;
   virtual ~PropertyOwner() = default;
-  static constexpr Kind KIND = kind_;
-  Kind kind() const override { return KIND; }
+  static constexpr AbstractPropertyOwner::Kind KIND = kind_;
+  AbstractPropertyOwner::Kind kind() const override { return KIND; }
 };
 
 std::ostream& operator<<(std::ostream& ostream, const AbstractPropertyOwner* apo);
