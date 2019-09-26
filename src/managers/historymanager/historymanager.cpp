5d64fc50a72a694a2da9a36a523ea9ffd002d9af
Pascal Bies <pascal.bies@k-lens.de>
2019-09-26 21:08:38 +0200

    fix bug in history manager

--- src/managers/historymanager/historymanager.cpp
+++ src/managers/historymanager/historymanager.cpp
@@ -19,10 +19,12 @@ HistoryManager::HistoryManager(Scene &scene)
     scene.history().set_index(index.row());
   });
   m_view->setSelectionMode(QAbstractItemView::NoSelection);
-  connect(&m_model, &HistoryModel::rowsInserted, [this](const QModelIndex&, int row) {
+  connect(&m_model, &HistoryModel::rowsInserted, this, [this](const QModelIndex&, int row) {
     m_view->scrollTo(m_model.index(row));
   });
-  connect(&m_model, &HistoryModel::dataChanged, [this](const QModelIndex& tl, const QModelIndex&) {
+  connect(&m_model, &HistoryModel::dataChanged, this,
+          [this](const QModelIndex& tl, const QModelIndex&)
+  {
     m_view->scrollTo(tl);
   });
 }
