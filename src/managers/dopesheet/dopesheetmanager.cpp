eeb9c5c12d37fc46cbb063fb5d74f57d5897631b
Pascal Bies <pascal.bies@k-lens.de>
2019-11-04 08:24:31 +0100

    fix DopeSheet-Bug

--- src/managers/dopesheet/dopesheetmanager.cpp
+++ src/managers/dopesheet/dopesheetmanager.cpp
@@ -13,7 +13,6 @@ DopeSheetManager::DopeSheetManager(Scene &scene)
   auto dope_sheet_view = std::make_unique<DopeSheetView>(scene.animator());
   m_dope_sheet_view = dope_sheet_view.get();
   set_widget(std::move(dope_sheet_view));
-  m_dope_sheet_view->setModel(&m_scene.animator());
 }
 
 bool DopeSheetManager::perform_action(const QString& name)
