c8b37e7140ebe8a72e1fdf64c1ade081c705a861
Pascal Bies <pascal.bies@k-lens.de>
2020-01-05 02:40:28 +0100

    Fix (de)compose nodes

--- src/managers/nodemanager/nodes/composenode.h
+++ src/managers/nodemanager/nodes/composenode.h
@@ -25,22 +25,4 @@ private:
   OutputPort* m_output_port;
 };
 
-class DecomposeNode : public Node
-{
-public:
-  explicit DecomposeNode(Scene* scene);
-  static constexpr auto INPUT_PROPERTY_KEY = "in";
-  static constexpr auto TYPE = QT_TRANSLATE_NOOP("any-context", "DecomposeNode");
-
-  QString definition() const override;
-  QString output_data_type(const OutputPort& port) const override;
-  QString title() const override;
-  bool accepts_input_data_type(const QString& type, const InputPort& port) const override;
-  QString type() const override { return TYPE; }
-
-private:
-  OutputPort* m_output_x_port;
-  OutputPort* m_output_y_port;
-};
-
 }  // namespace omm
--- src/managers/nodemanager/nodes/decomposenode.h
+++ src/managers/nodemanager/nodes/decomposenode.h
@@ -7,24 +7,6 @@ namespace omm
 
 template<PortType> class OrdinaryPort;
 
-class ComposeNode : public Node
-{
-public:
-  explicit ComposeNode(Scene* scene);
-  static constexpr auto INPUT_X_PROPERTY_KEY = "x";
-  static constexpr auto INPUT_Y_PROPERTY_KEY = "y";
-  static constexpr auto TYPE = QT_TRANSLATE_NOOP("any-context", "ComposeNode");
-
-  QString definition() const override;
-  QString output_data_type(const OutputPort& port) const override;
-  QString title() const override;
-  bool accepts_input_data_type(const QString& type, const InputPort& port) const override;
-  QString type() const override { return TYPE; }
-
-private:
-  OutputPort* m_output_port;
-};
-
 class DecomposeNode : public Node
 {
 public:
