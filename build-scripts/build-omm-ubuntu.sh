5fad8440fc9ffe91dbd82a8210025d64f79b15d3
Pascal Bies <pascal.bies@k-lens.de>
2020-04-27 10:43:11 +0200

    maybe fix CI-bug

--- build-scripts/build-omm-ubuntu.sh
+++ build-scripts/build-omm-ubuntu.sh
@@ -42,7 +42,7 @@ case "$dist" in
   tar xf Python-3.7.0.tar.xz
   pushd Python-3.7.0
   ./configure --prefix=/usr
-  sudo make install -j2
+  sudo make altinstall -j2
   popd
 
   # pytest is required to build pybind11
