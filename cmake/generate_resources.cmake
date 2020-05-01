54e07c1381966f2355d373ea118945b1a03ca53a
Pascal Bies <pascal.bies@k-lens.de>
2020-05-01 12:33:31 +0200

    re-generate resources when keybindings.cfg has changed

--- cmake/generate_resources.cmake
+++ cmake/generate_resources.cmake
@@ -1,7 +1,9 @@
 function(generate_resources target compiled_resource_file qrc_files)
   add_custom_command(
       OUTPUT "${compiled_resource_file}"
-      DEPENDS ${qrc_files}
+      DEPENDS
+        ${qrc_files}
+        "${CMAKE_CURRENT_SOURCE_DIR}/keybindings/default_keybindings.cfg"
       COMMAND_EXPAND_LISTS
       COMMAND Qt5::rcc
         -o "${compiled_resource_file}" ${qrc_files}
