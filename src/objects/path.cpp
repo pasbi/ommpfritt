c821463db167f27ba31d90a4bea88b72fa81ed91
Pascal Bies <pascal.bies@k-lens.de>
2020-05-24 09:39:14 +0200

    implement multi-segment path

--- src/objects/path.cpp
+++ src/objects/path.cpp
@@ -9,46 +9,14 @@
 #include "common.h"
 #include "renderers/style.h"
 
-namespace
-{
-
-static constexpr auto POSITION_POINTER = "position";
-static constexpr auto LEFT_TANGENT_POINTER = "left";
-static constexpr auto RIGHT_TANGENT_POINTER = "right";
-
-template<typename Pointer>
-auto deserialize_point(omm::AbstractDeserializer& deserializer, const Pointer& pointer)
-{
-  const auto make_pointer = [&pointer](const auto& top_level_pointer) {
-    return omm::Serializable::make_pointer(pointer, top_level_pointer);
-  };
-  omm::Point point;
-  point.position = deserializer.get_vec2f(make_pointer(POSITION_POINTER));
-  point.left_tangent = deserializer.get_polarcoordinates(make_pointer(LEFT_TANGENT_POINTER));
-  point.right_tangent = deserializer.get_polarcoordinates(make_pointer(RIGHT_TANGENT_POINTER));
-  return point;
-}
-
-template<typename Pointer>
-void serialize_point( omm::AbstractSerializer& serializer, const Pointer& pointer,
-                      const omm::Point& point )
-{
-  const auto make_pointer = [&pointer](const auto& top_level_pointer) {
-    return omm::Serializable::make_pointer(pointer, top_level_pointer);
-  };
-  serializer.set_value(point.position, make_pointer(POSITION_POINTER));
-  serializer.set_value(point.left_tangent, make_pointer(LEFT_TANGENT_POINTER));
-  serializer.set_value(point.right_tangent, make_pointer(RIGHT_TANGENT_POINTER));
-}
-
-}  // namespace
-
 namespace omm
 {
 
 class Style;
 
-Path::Path(Scene* scene) : Object(scene)
+Path::Path(Scene* scene)
+  : Object(scene)
+  , painter_path(*this)
 {
   static const auto category = QObject::tr("path");
 
@@ -64,23 +32,23 @@ Path::Path(Scene* scene) : Object(scene)
 
 void Path::draw_object(Painter &renderer, const Style& style, Painter::Options options) const
 {
-//  renderer.set_style(style, *this, options);
-//  renderer.painter->drawPath(m_painter_path);
-//  const auto marker_color = style.property(Style::PEN_COLOR_KEY)->value<Color>();
-//  const auto width = style.property(Style::PEN_WIDTH_KEY)->value<double>();
-//  style.start_marker.draw_marker(renderer, evaluate(0.0).rotated(0.5 * M_PI), marker_color, width);
-//  style.end_marker.draw_marker(renderer, evaluate(1.0).rotated(1.5 * M_PI), marker_color, width);
+  renderer.set_style(style, *this, options);
+  renderer.painter->drawPath(painter_path());
+  const auto marker_color = style.property(Style::PEN_COLOR_KEY)->value<Color>();
+  const auto width = style.property(Style::PEN_WIDTH_KEY)->value<double>();
+  style.start_marker.draw_marker(renderer, evaluate(0.0).rotated(0.5 * M_PI), marker_color, width);
+  style.end_marker.draw_marker(renderer, evaluate(1.0).rotated(1.5 * M_PI), marker_color, width);
 }
 
 BoundingBox Path::bounding_box(const ObjectTransformation &transformation) const
 {
   Q_UNUSED(transformation);
   return BoundingBox();
-//  if (is_active()) {
-//    return BoundingBox((m_painter_path * transformation.to_qtransform()).boundingRect());
-//  } else {
-//    return BoundingBox();
-//  }
+  if (is_active()) {
+    return BoundingBox((painter_path() * transformation.to_qtransform()).boundingRect());
+  } else {
+    return BoundingBox();
+  }
 }
 
 QString Path::type() const { return TYPE; }
@@ -89,36 +57,93 @@ void Path::serialize(AbstractSerializer& serializer, const Pointer& root) const
 {
   Object::serialize(serializer, root);
 
-//  const auto points_pointer = make_pointer(root, POINTS_POINTER);
-//  serializer.start_array(m_points.size(), points_pointer);
-//  for (size_t i = 0; i < m_points.size(); ++i) {
-//    const auto point_pointer = make_pointer(points_pointer, i);
-//    serialize_point(serializer, point_pointer, m_points[i]);
-//  }
-//  serializer.end_array();
+  const auto subpath_ptr = make_pointer(root, SUBPATH_POINTER);
+  serializer.start_array(m_segments.size(), subpath_ptr);
+  for (std::size_t i = 0; i < m_segments.size(); ++i) {
+    const auto pts_ptr = make_pointer(subpath_ptr, i);
+    serializer.start_array(m_segments.size(), subpath_ptr);
+    std::size_t j = 0;
+    for (auto&& point : m_segments[i]) {
+      point.serialize(serializer, make_pointer(pts_ptr, j));
+      j += 1;
+    }
+    serializer.end_array();
+  }
+  serializer.end_array();
 }
 
 void Path::deserialize(AbstractDeserializer& deserializer, const Pointer& root)
 {
   Object::deserialize(deserializer, root);
 
-//  const auto points_pointer = make_pointer(root, POINTS_POINTER);
-//  size_t n_points = deserializer.array_size(points_pointer);
-//  m_points = std::vector<Point>(n_points);
-//  for (size_t i = 0; i < n_points; ++i) {
-//    const auto point_pointer = make_pointer(points_pointer, i);
-//    m_points[i] = deserialize_point(deserializer, point_pointer);
-//  }
-//  update();
+  const auto subpath_ptr = make_pointer(root, SUBPATH_POINTER);
+  const std::size_t n_paths = deserializer.array_size(subpath_ptr);
+  m_segments.clear();
+  m_segments.reserve(n_paths);
+  for (size_t i = 0; i < n_paths; ++i) {
+    const auto pts_ptr = make_pointer(subpath_ptr, i);
+    deserializer.array_size(pts_ptr);
+    m_segments.push_back({});
+    for (size_t j = 0; j < n_paths; ++j) {
+      Point p;
+      p.deserialize(deserializer, make_pointer(pts_ptr, j));
+      m_segments[i].push_back(p);
+    }
+  }
+  update();
 }
 
 void Path::update()
 {
-//  m_painter_path = Painter::path(m_points, property(IS_CLOSED_PROPERTY_KEY)->value<bool>());
+  painter_path.invalidate();
   Object::update();
+}
+
+void Path::insert(const const_iterator& pos, const Segment& points)
+{
+  assert(!m_segments.empty());
+  if (pos.segment_iterator() == m_segments.end()) {
+    m_segments.push_back(Segment{});
+    m_segments.back().insert(m_segments.back().begin(), points.begin(), points.end());
+  } else {
+    const auto ncpos = remove_const(pos);
+    Segment& segment = *ncpos.segment_iterator();
+    segment.insert(ncpos.point_iterator(), points.begin(), points.end());
+  }
 }
 
 Flag Path::flags() const { return Object::flags() | Flag::IsPathLike; }
 
+QPainterPath Path::CachedQPainterPathGetter::compute() const
+{
+  static const auto p = [](const Vec2f& v) { return QPointF{v.x, v.y}; };
+  QPainterPath path;
+  for (auto&& points : m_self.m_segments) {
+    if (!points.empty()) {
+      path.moveTo(p(points.front().position));
+    }
+    auto previous = points.begin();
+    auto current = previous;
+    ++current;
+    while (current != points.end()) {
+      path.cubicTo(p(previous->right_position()),
+                   p(current->left_position()),
+                   p(current->position));
+      previous = current;
+      ++current;
+    }
+    if (m_self.is_closed()) {
+      path.cubicTo(p(points.back().right_position()),
+                   p(points.front().left_position()),
+                   p(points.front().position));
+    }
+  }
+  return path;
+}
+
+Path::const_iterator begin(const Path& path) { return path.begin(); }
+Path::const_iterator end(const Path& path) { return path.end(); }
+Path::iterator begin(Path& path) { return path.begin(); }
+Path::iterator end(Path& path) { return path.end(); }
 
 }  // namespace omm
--- src/objects/path.h
+++ src/objects/path.h
@@ -4,6 +4,7 @@
 #include "geometry/point.h"
 #include <list>
 #include "geometry/cubics.h"
+#include "cachedgetter.h"
 
 namespace omm
 {
@@ -20,7 +21,7 @@ public:
 
   static constexpr auto TYPE = QT_TRANSLATE_NOOP("any-context", "Path");
   static constexpr auto IS_CLOSED_PROPERTY_KEY = "closed";
-  static constexpr auto POINTS_POINTER = "points";
+  static constexpr auto SUBPATH_POINTER = "paths";
   static constexpr auto INTERPOLATION_PROPERTY_KEY = "interpolation";
 
   void serialize(AbstractSerializer& serializer, const Pointer& root) const override;
@@ -28,11 +29,158 @@ public:
 
   enum class InterpolationMode { Linear, Smooth, Bezier };
   Flag flags() const override;
-
   void update() override;
 
-private:
+  using Segment = std::list<Point>;
 
+private:
+public:
+  std::vector<Segment> m_segments = { Segment{} };
+
+  struct CachedQPainterPathGetter : CachedGetter<QPainterPath, Path>
+  {
+    using CachedGetter::CachedGetter;
+  private:
+    QPainterPath compute() const override;
+  } painter_path;
+  friend struct CachedQPainterPathGetter;
+
+  template<bool Const>
+  struct Iterator
+  {
+  public:
+    using difference_type = int;
+    using value_type = Path;
+    using pointer = std::conditional_t<Const, const Path*, Path*>;
+    using reference = std::conditional_t<Const, const Path& , Path&>;
+    using iterator_category = std::bidirectional_iterator_tag;
+
+    using point_iterator_type = std::conditional_t<Const, Segment::const_iterator,
+                                                          Segment::iterator>;
+    using segment_iterator_type = std::conditional_t<Const, std::vector<Segment>::const_iterator,
+                                                            std::vector<Segment>::iterator>;
+    explicit Iterator(reference path, const segment_iterator_type& segment_iterator,
+                                      const point_iterator_type& point_iterator)
+      : m_path(path)
+      , m_segment_iterator(segment_iterator)
+      , m_point_iterator(point_iterator)
+    {
+    }
+
+    static auto begin(reference path)
+    {
+      assert(!path.m_segments.empty());
+      return Iterator{path, path.m_segments.begin(), path.m_segments.front().begin()};
+    }
+
+    static auto end(reference path)
+    {
+      assert(!path.m_segments.empty());
+      return Iterator(path, path.m_segments.end() - 1, path.m_segments.back().end());
+    }
+
+    bool operator==(const Iterator& other) const
+    {
+      LINFO << (this->m_point_iterator == other.m_point_iterator);
+      return &this->m_path == &other.m_path
+          && this->m_segment_iterator == other.m_segment_iterator
+          && this->m_point_iterator == other.m_point_iterator;
+    }
+
+    bool operator!=(const Iterator& other) const
+    {
+      return !(*this == other);
+    }
+
+    Iterator& operator--()
+    {
+      if (m_point_iterator == m_segment_iterator->begin()) {
+        --m_segment_iterator;
+        m_point_iterator == m_segment_iterator->end();
+      } else {
+        --m_point_iterator;
+      }
+      return *this;
+    }
+
+    Iterator& operator++()
+    {
+      ++m_point_iterator;
+      if (m_point_iterator == m_segment_iterator->end()) {
+        ++m_segment_iterator;
+        if (m_segment_iterator == m_path.m_segments.end()) {
+          --m_segment_iterator;
+          m_point_iterator = m_segment_iterator->end();
+        } else {
+          m_point_iterator = m_segment_iterator->begin();
+        }
+      }
+      return *this;
+    }
+
+    decltype(auto) operator*() const
+    {
+      return *m_point_iterator;
+    }
+
+    operator Iterator<true>() const
+    {
+      return Iterator<true>{m_path, m_segment_iterator, m_point_iterator};
+    }
+
+    point_iterator_type point_iterator() const
+    {
+      return m_point_iterator;
+    }
+
+    segment_iterator_type segment_iterator() const
+    {
+      return m_segment_iterator;
+    }
+
+    bool is_segment_begin() const
+    {
+      return m_point_iterator == m_segment_iterator->begin();
+    }
+
+    bool is_segment_ultimo() const
+    {
+      auto end = m_segment_iterator->end();
+      --end;
+      return m_point_iterator == end;
+    }
+
+  private:
+    reference m_path;
+    segment_iterator_type m_segment_iterator;
+    point_iterator_type m_point_iterator;
+    friend class Path;
+  };
+
+  template<bool> friend struct Iterator;
+
+public:
+  using iterator = Iterator<false>;
+  using const_iterator = Iterator<true>;
+  decltype(auto) begin() { return iterator::begin(*this); }
+  decltype(auto) end() { return iterator::end(*this); }
+  decltype(auto) begin() const { return const_iterator::begin(*this); }
+  decltype(auto) end() const { return const_iterator::end(*this); }
+  iterator remove_const(const const_iterator& it)
+  {
+    assert(&it.m_path == this);
+    const auto seg_it = m_segments.erase(it.m_segment_iterator, it.m_segment_iterator);
+    const auto pt_it = seg_it->erase(it.m_point_iterator, it.m_point_iterator);
+    return iterator{*this, seg_it, pt_it};
+  }
+
+  void insert(const const_iterator& pos, const Segment& points);
+  const auto& segments() const { return m_segments; }
 };
+
+Path::const_iterator begin(const Path& path);
+Path::const_iterator end(const Path& path);
+Path::iterator begin(Path& path);
+Path::iterator end(Path& path);
 
 }  // namespace omm
