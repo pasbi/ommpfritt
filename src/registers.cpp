e65b48785bfb034d5702090e2b84ea05c2471a8e
Pascal Bies <pascal.bies@k-lens.de>
2020-07-04 20:24:26 +0200

    enforce unique name for Manager, Object, Tag, Property, Tool and Node types.

--- src/registers.cpp
+++ src/registers.cpp
@@ -1,5 +1,6 @@
 #include "registers.h"
 #include "logging.h"
+#include <list>
 
 #include "register_managers.cpp"
 #include "register_objects.cpp"
@@ -8,17 +9,47 @@
 #include "register_tools.cpp"
 #include "register_nodes.cpp"
 
+namespace
+{
+
+template<typename Ts>
+void merge(std::list<QString>& accu, Ts&& ts)
+{
+  accu.insert(accu.end(), ts.begin(), ts.end());
+}
+
+}  // namespace
+
 namespace omm
 {
 
 void register_everything()
 {
+  std::list<QString> keys;
   register_managers();
+  merge(keys, Manager::keys());
   register_objects();
+  merge(keys, Object::keys());
   register_tags();
+  merge(keys, Tag::keys());
   register_properties();
+  merge(keys, Property::keys());
   register_tools();
+  merge(keys, Tool::keys());
   register_nodes();
+  merge(keys, Node::keys());
+
+  // assume that the keys are unique, even for different types.
+  // However, keys may have the same name as actions.
+  for (auto it = keys.begin(); it != keys.end(); ++it) {
+    for (auto it2 = keys.begin(); it2 != it; ++it2) {
+      if (*it == *it2) {
+        LERROR << "Duplicate key: " << *it;
+        LFATAL("Duplicate key");
+      }
+    }
+  }
+
 }
 
 }  // namespace omm
