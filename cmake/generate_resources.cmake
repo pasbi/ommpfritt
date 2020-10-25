63c5030220f126c1a856cfe6fcd34b1419ab61ee
Pascal Bies <pascal.bies@k-lens.de>
2020-10-25 16:34:13 +0100

    fix dependency build order

--- cmake/generate_resources.cmake
+++ cmake/generate_resources.cmake
@@ -1,15 +1,13 @@
 function(generate_resources target compiled_resource_file qrc_files)
-  add_custom_command(
-      OUTPUT "${compiled_resource_file}"
+  add_custom_target(${target}
+      COMMAND Qt5::rcc -o "${compiled_resource_file}" ${qrc_files}
+      COMMAND_EXPAND_LISTS
+      BYPRODUCTS "${compiled_resource_file}"
       DEPENDS ${qrc_files} ${source_files}
         "${CMAKE_CURRENT_SOURCE_DIR}/keybindings/default_keybindings.cfg"
         "${CMAKE_CURRENT_SOURCE_DIR}/layouts/default_layout.ini"
-      COMMAND_EXPAND_LISTS
-      COMMAND Qt5::rcc
-        -o "${compiled_resource_file}" ${qrc_files}
       COMMENT "Compile ${compiled_resource_file} from ${qrc_files}."
-  )
-  add_custom_target(${target} DEPENDS "${compiled_resource_file}")
+      )
   add_dependencies(${target} translations_qm)
 endfunction()
 
