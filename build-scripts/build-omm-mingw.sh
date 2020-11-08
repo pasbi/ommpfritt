b3529118652e1ac2203bcd110794bb7e926a641f
Pascal Bies <pascal.bies@k-lens.de>
2020-11-08 17:21:43 +0100

    move dependencies install instructions
    only for mingw build (pacman).
    On ubuntu, it's quite complicated to set the variables right, and I see no immediate benefit.

--- build-scripts/build-omm-mingw.sh
+++ build-scripts/build-omm-mingw.sh
@@ -5,31 +5,6 @@
 set -e
 cd "$(dirname "$0")/.."
 
-echo "Installing required packages ..."
-pacman --noconfirm --needed -S \
-    make \
-    python \
-    mingw-w64-x86_64-ninja \
-    mingw-w64-x86_64-qt5 \
-    mingw-w64-x86_64-poppler \
-    mingw-w64-x86_64-python \
-    mingw-w64-x86_64-kitemmodels-qt5 \
-    mingw-w64-x86_64-openssl \
-    mingw-w64-x86_64-libffi \
-    mingw-w64-x86_64-zlib \
-    mingw-w64-x86_64-python-pytest \
-    mingw-w64-x86_64-dlfcn \
-    mingw-w64-x86_64-nsis \
-    mingw-w64-x86_64-boost \
-    mingw-w64-x86_64-double-conversion \
-    mingw-w64-x86_64-gsl \
-    mingw-w64-x86_64-cython \
-    mingw-w64-x86_64-gtk3 \
-    mingw-w64-x86_64-cmake \
-    mingw-w64-x86_64-mesa \
-    mingw-w64-x86_64-gtkmm3 \
-    mingw-w64-x86_64-imagemagick
-
 echo "INSTALLED PACKAGES:"
 pacman -Q
 echo "END INSTALLED PACKAGES."
