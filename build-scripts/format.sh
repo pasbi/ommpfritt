ed4c5e2cd9241dc4696d094a6fba45e2605345fe
Pascal Bies <pascal.bies@k-lens.de>
2020-11-14 22:55:14 +0100

    improve formatting script

--- build-scripts/format.sh
+++ build-scripts/format.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)/..
+cd "$(dirname $0)/.."
 
 files=$(find src -path src/external -prune -false -o -type f \( -iname \*.cpp -o -iname \*.h \))
 
@@ -8,7 +8,7 @@ case "$1" in
 "check")
   bad_files=0
   for f in $files; do
-    d=$(diff --old-line-format="%5dn < %L" --new-line-format="%5dn > %L" --unchanged-line-format="" $f <(clang-format $f))
+    d=$(diff --old-line-format="%5dn < %L" --new-line-format="%5dn > %L" --unchanged-line-format="" "$f" <(clang-format "$f"))
     if [ $? -ne 0 ]; then
       echo "ERROR: $f has wrong format!"
       echo -e "$d"
@@ -24,7 +24,7 @@ case "$1" in
 ;;
 "apply")
   for f in $files; do
-    clang-format -i $f
+    clang-format -i "$f"
   done
 ;;
 esac
