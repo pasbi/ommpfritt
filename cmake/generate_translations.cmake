f5cef22cd6d2eb182b99c328829acc5f6d8a5cd3
Pascal Bies <pascal.bies@k-lens.de>
2020-11-20 17:53:47 +0100

    remove annoying cmake cycle
    fix #117

--- cmake/generate_translations.cmake
+++ cmake/generate_translations.cmake
@@ -6,14 +6,20 @@ function(generate_translations translations_qrc ts_dir languages prefixes cfg_fi
     list(TRANSFORM ts_files PREPEND "${ts_dir}/omm_")
     set(script "${CMAKE_CURRENT_SOURCE_DIR}/build-scripts/update-translations_h.py")
     set(translations_h "${CMAKE_CURRENT_SOURCE_DIR}/src/translations.h")
-    file(GLOB_RECURSE TS_SOURCES "*.cpp" "*.h" "*.ui")
+    #file(GLOB_RECURSE TS_SOURCES "*.cpp" "*.h" "*.ui")
+    file(GLOB_RECURSE TS_SOURCES
+         CONFIGURE_DEPENDS
+         "${CMAKE_SOURCE_DIR}/src/*.cpp"
+         "${CMAKE_SOURCE_DIR}/src/*.h"
+         "${CMAKE_SOURCE_DIR}/src/*.ui"
+    )
     list(REMOVE_ITEM TS_SOURCES "${translations_h}")
 
-    add_custom_command(
-        OUTPUT "${translations_h}"
+    add_custom_target(
+        ts_target
+        BYPRODUCTS "${translations_h}" "${ts_files}"
         DEPENDS ${cfg_files} "${script}" ${TS_SOURCES}
         COMMAND_EXPAND_LISTS
-        COMMAND touch /tmp/x
         COMMAND Python3::Interpreter "${script}"
           --input "${cfg_files}"
           --output "${translations_h}"
@@ -23,12 +29,12 @@ function(generate_translations translations_qrc ts_dir languages prefixes cfg_fi
           -ts "${ts_files}"
         COMMENT "Update ts files. Check the files with `linguist` manually!"
     )
-    add_custom_target(ts_target DEPENDS "${translations_h}" "${ts_files}")
 
     set(script "${CMAKE_CURRENT_SOURCE_DIR}/build-scripts/generate-translations_qrc.py")
-    add_custom_command(
-        OUTPUT "${qm_dir}/translations.qrc"
+    add_custom_target(
+        translations_qm
         DEPENDS ts_target
+        BYPRODUCTS "${translations_qrc}"
         COMMAND_EXPAND_LISTS
         COMMAND Python3::Interpreter "${script}"
           --languages "${languages}"
@@ -36,8 +42,6 @@ function(generate_translations translations_qrc ts_dir languages prefixes cfg_fi
           --qrc "${translations_qrc}"
         COMMENT "generate translations.qrc"
     )
-
-    add_custom_target(translations_qm DEPENDS "${translations_qrc}" )
 
     set_source_files_properties("${translations_qrc}" PROPERTIES SKIP_AUTORCC ON)
 
@@ -49,15 +53,15 @@ function(generate_translations translations_qrc ts_dir languages prefixes cfg_fi
         list(TRANSFORM qt_qm_files PREPEND "${qm_dir}/" OUTPUT_VARIABLE dst_qt_qm_files)
         list(TRANSFORM qt_qm_files PREPEND "${QT_QM_PATH}/" OUTPUT_VARIABLE src_qt_qm_files)
 
-        add_custom_command(
-            OUTPUT "${qm}" ${dst_qt_qm_files}
+        add_custom_target(
+            "qm_${lang}"
             DEPENDS ${TS_SOURCES}
+            BYPRODUCTS ${qm} ${dst_qt_qm_files}
             COMMAND Qt5::lrelease ${ts} -qm ${qm}
             COMMAND ${CMAKE_COMMAND} -E copy_if_different
                     ${src_qt_qm_files} "${qm_dir}"
             COMMENT "Prepare translation files '${lang}'"
         )
-        add_custom_target("qm_${lang}" DEPENDS ${qm} ${dst_qt_qm_files} )
         add_dependencies(translations_qm "qm_${lang}")
     endforeach()
 endfunction()
