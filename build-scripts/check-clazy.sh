d850a803ab309cb49292ff8f2466081292667bca
Pascal Bies <pascal.bies@k-lens.de>
2020-11-23 22:58:34 +0100

    disable connect-non-signal
    they say it has no false positives, but there were.

--- build-scripts/check-clazy.sh
+++ build-scripts/check-clazy.sh
@@ -2,8 +2,34 @@
 
 cd "$(dirname "$0")/.."
 
-clazy_checks= \
-level0,\
+clazy_checks=\
+connect-by-name,\
+connect-not-normalized,\
+container-anti-pattern,\
+empty-qstringliteral,\
+fully-qualified-moc-types,\
+lambda-in-connect,\
+lambda-unique-connection,\
+lowercase-qml-type-name,\
+mutable-container-key,\
+overloaded-signal,\
+qcolor-from-literal,\
+qdatetime-utc,\
+qenums,\
+qfileinfo-exists,\
+qgetenv,\
+qmap-with-pointer-key,\
+qstring-arg,\
+qstring-comparison-to-implicit-char,\
+qstring-insensitive-allocation,\
+qstring-ref,\
+qt-macros,\
+strict-iterators,\
+temporary-iterator,\
+unused-non-trivial-variable,\
+writing-to-temporary,\
+wrong-qevent-cast,\
+wrong-qglobalstatic,\
 auto-unexpected-qstringbuilder,\
 child-event-qobject-cast,\
 connect-3arg-lambda,\
@@ -23,11 +49,12 @@ range-loop,\
 returning-data-from-temporary,\
 rule-of-two-soft,\
 skipped-base-method,\
-virtual-signal\
+virtual-signal
 
 # Enable all checks from level0 and all checks from level1 except:
-# "incorrect-emit"            # yields false positives
-# "non-pod-global-static"     # having many non-pod-global-static objects is an open issue.
+# incorrect-emit            # false positives
+# connect-non-signal        # many false positives
+# non-pod-global-static     # having many non-pod-global-static objects is an open issue.
 
 echo "clazy checks: $clazy_checks"
 
