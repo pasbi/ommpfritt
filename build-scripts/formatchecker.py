2e7f1ac71530e532fd420b39232fd55ec8c283e9
Pascal Bies <pascal.bies@k-lens.de>
2021-02-07 23:21:11 +0100

    add doc-string

--- build-scripts/formatchecker.py
+++ build-scripts/formatchecker.py
@@ -6,6 +6,12 @@ import re
 
 class Checker:
     def __init__(self, filename):
+        """Loads a file with given filename and constructs an extensible Checker class.
+           Define your checker member methods.
+           Then override self.line_based_checks and call them.
+           Each checker must call self.error with a sensibe message, if a formatting error was
+           detected that must be fixed, or self.warning for problems that are optional to fix.
+        """
         with open(filename, 'rb') as f:
             # open as binary, otherwise \r\n will be hidden.
             self.lines = f.read().decode('utf-8').split("\n")
