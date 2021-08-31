0e3c9179c4e596cab5470530fe705efb24437b99
Pascal Bies <pascal.bies@k-lens.de>
2021-08-31 08:44:56 +0200

    minor, unrelated refactoring

--- test/unit/iotest.cpp
+++ test/unit/iotest.cpp
@@ -38,8 +38,9 @@ std::unique_ptr<omm::Application> get_application(int kill_after_msec = 1)
 
 TEST(IOTest, load_icons)
 {
+  static constexpr auto kill_after_msec = 1000;
   using namespace std::string_literals;
-  auto app = get_application(1000);
+  auto app = get_application(kill_after_msec);
   static const auto fn = QString::fromStdString(ommtest::SOURCE_DIRECTORY + "/icons/icons.omm"s);
   EXPECT_TRUE(app->scene.load_from(fn));
 }
