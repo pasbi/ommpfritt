80c29aaa35800173fe839ad57fd4a8305fab29c8
Pascal Bies <pascal.bies@k-lens.de>
2021-11-04 09:08:45 +0100

    restore SelectSimilarTool

--- src/tools/selectsimilartool.cpp
+++ src/tools/selectsimilartool.cpp
@@ -1,183 +1,188 @@
-//#include "tools/selectsimilartool.h"
-//#include "common.h"
-//#include "logging.h"
-//#include "objects/path.h"
-//#include "properties/floatproperty.h"
-//#include "properties/optionproperty.h"
-//#include "properties/triggerproperty.h"
-//#include "scene/mailbox.h"
-//#include "scene/scene.h"
-//#include "tools/handles/boundingboxhandle.h"
+#include "tools/selectsimilartool.h"
+#include "common.h"
+#include "logging.h"
+#include "objects/path.h"
+#include "properties/floatproperty.h"
+#include "properties/optionproperty.h"
+#include "properties/triggerproperty.h"
+#include "scene/mailbox.h"
+#include "scene/scene.h"
+#include "tools/handles/boundingboxhandle.h"
 
-//namespace
-//{
-//constexpr auto MODE_PROPERTY_KEY = "mode";
-//constexpr auto STRATEGY_PROPERTY_KEY = "strategy";
-//constexpr auto THRESHOLD_PROPERTY_KEY = "threshold";
-//constexpr auto APPLY_PROPERTY_KEY = "apply";
-//enum class Mode { Normal, X, Y, Distance };
-//enum class MatchStrategy { Any, All };
-//using It = omm::PathIterator;
+namespace
+{
+constexpr auto MODE_PROPERTY_KEY = "mode";
+constexpr auto STRATEGY_PROPERTY_KEY = "strategy";
+constexpr auto THRESHOLD_PROPERTY_KEY = "threshold";
+constexpr auto APPLY_PROPERTY_KEY = "apply";
+enum class Mode { Normal, X, Y, Distance };
+enum class MatchStrategy { Any, All };
+using namespace omm;
 
-//double normal_distance(const It& a, const It& b)
-//{
-//  const auto normalize = [](double a, double b) {
-//    return omm::M_180_PI * omm::PolarCoordinates::normalize_angle(std::abs(a - b)) - M_PI_2;
-//  };
+double normal_distance(const Point& a, const Point& b)
+{
+  const auto normalize = [](double a, double b) {
+    return M_180_PI * PolarCoordinates::normalize_angle(std::abs(a - b)) - M_PI_2;
+  };
 
-//  return std::min(normalize(a->left_tangent.argument, b->left_tangent.argument),
-//                  normalize(a->right_tangent.argument, b->right_tangent.argument));
-//}
+  return std::min(normalize(a.left_tangent.argument, b.left_tangent.argument),
+                  normalize(a.right_tangent.argument, b.right_tangent.argument));
+}
 
-//omm::Vec2f distance(const It& a, const It& b, omm::SelectSimilarTool::Alignment alignment)
-//{
-//  if (alignment == omm::SelectSimilarTool::Alignment::Global) {
-//    const auto global_pos = [](const It& it) {
-//      const auto t = it.path->global_transformation(omm::Space::Viewport);
-//      return t.apply_to_position(it->position);
-//    };
-//    return global_pos(a) - global_pos(b);
-//  } else {
-//    return a->position - b->position;
-//  }
-//}
+omm::Vec2f distance(const Path& path, const Point& a, const Point& b, omm::SelectSimilarTool::Alignment alignment)
+{
+  if (alignment == SelectSimilarTool::Alignment::Global) {
+    const auto global_pos = [&path](const Point& point) {
+      const auto t = path.global_transformation(omm::Space::Viewport);
+      return t.apply_to_position(point.position);
+    };
+    return global_pos(a) - global_pos(b);
+  } else {
+    return a.position - b.position;
+  }
+}
 
-//}  // namespace
+}  // namespace
 
-//namespace omm
-//{
-//SelectSimilarTool::SelectSimilarTool(Scene& scene) : SelectPointsBaseTool(scene)
-//{
-//  static constexpr double THRESHOLD_STEP = 0.1;
-//  const auto category = QObject::tr("tool");
-//  create_property<OptionProperty>(MODE_PROPERTY_KEY, 0)
-//      .set_options(
-//          {QObject::tr("normal"), QObject::tr("x"), QObject::tr("y"), QObject::tr("distance")})
-//      .set_label(QObject::tr("similarity"))
-//      .set_category(category)
-//      .set_animatable(false);
-//  create_property<OptionProperty>(STRATEGY_PROPERTY_KEY, 0)
-//      .set_options({QObject::tr("Any"), QObject::tr("All")})
-//      .set_label(QObject::tr("strategy"))
-//      .set_category(category)
-//      .set_animatable(false);
-//  create_property<FloatProperty>(THRESHOLD_PROPERTY_KEY, 1.0)
-//      .set_step(THRESHOLD_STEP)
-//      .set_label(QObject::tr("threshold"))
-//      .set_category(category)
-//      .set_animatable(false);
-//  create_property<TriggerProperty>(APPLY_PROPERTY_KEY)
-//      .set_label(QObject::tr("apply"))
-//      .set_category(category)
-//      .set_animatable(false);
-//  connect(&scene.mail_box(),
-//          &MailBox::point_selection_changed,
-//          this,
-//          &SelectSimilarTool::update_base_selection);
-//  update_property_appearance();
-//}
+namespace omm
+{
+SelectSimilarTool::SelectSimilarTool(Scene& scene) : SelectPointsBaseTool(scene)
+{
+  static constexpr double THRESHOLD_STEP = 0.1;
+  const auto category = QObject::tr("tool");
+  create_property<OptionProperty>(MODE_PROPERTY_KEY, 0)
+      .set_options(
+          {QObject::tr("normal"), QObject::tr("x"), QObject::tr("y"), QObject::tr("distance")})
+      .set_label(QObject::tr("similarity"))
+      .set_category(category)
+      .set_animatable(false);
+  create_property<OptionProperty>(STRATEGY_PROPERTY_KEY, 0)
+      .set_options({QObject::tr("Any"), QObject::tr("All")})
+      .set_label(QObject::tr("strategy"))
+      .set_category(category)
+      .set_animatable(false);
+  create_property<FloatProperty>(THRESHOLD_PROPERTY_KEY, 1.0)
+      .set_step(THRESHOLD_STEP)
+      .set_label(QObject::tr("threshold"))
+      .set_category(category)
+      .set_animatable(false);
+  create_property<TriggerProperty>(APPLY_PROPERTY_KEY)
+      .set_label(QObject::tr("apply"))
+      .set_category(category)
+      .set_animatable(false);
+  connect(&scene.mail_box(),
+          &MailBox::point_selection_changed,
+          this,
+          &SelectSimilarTool::update_base_selection);
+  update_property_appearance();
+}
 
-//void SelectSimilarTool::reset()
-//{
-//  update_base_selection();
-//  handles.clear();
-//  make_handles(*this, false);
-//  handles.push_back(std::make_unique<BoundingBoxHandle<SelectSimilarTool>>(*this));
-//}
+void SelectSimilarTool::reset()
+{
+  update_base_selection();
+  handles.clear();
+  make_handles(*this, false);
+  handles.push_back(std::make_unique<BoundingBoxHandle<SelectSimilarTool>>(*this));
+}
 
-//void SelectSimilarTool::on_property_value_changed(Property* property)
-//{
-//  update_property_appearance();
-//  if (pmatch(property, {APPLY_PROPERTY_KEY})) {
-//    reset();
-//  } else if (pmatch(property, {THRESHOLD_PROPERTY_KEY, MODE_PROPERTY_KEY, STRATEGY_PROPERTY_KEY})) {
-//    update_selection();
-//  } else {
-//    SelectPointsBaseTool::on_property_value_changed(property);
-//  }
-//}
+void SelectSimilarTool::on_property_value_changed(Property* property)
+{
+  update_property_appearance();
+  if (pmatch(property, {APPLY_PROPERTY_KEY})) {
+    reset();
+  } else if (pmatch(property, {THRESHOLD_PROPERTY_KEY, MODE_PROPERTY_KEY, STRATEGY_PROPERTY_KEY})) {
+    update_selection();
+  } else {
+    SelectPointsBaseTool::on_property_value_changed(property);
+  }
+}
 
-//void SelectSimilarTool::update_selection()
-//{
-//  const auto strategy = property(STRATEGY_PROPERTY_KEY)->value<MatchStrategy>();
-//  for (auto&& path : scene()->item_selection<Path>()) {
-//    for (auto&& it = path->begin(); it != path->end(); ++it) {
-//      if (!::contains(m_base_selection, it)) {
-//        it->is_selected = [this, strategy, &it] {
-//          const auto is_similar = [&it, this](auto&& b) { return this->is_similar(it, b); };
-//          switch (strategy) {
-//          case MatchStrategy::All:
-//            return std::all_of(m_base_selection.begin(), m_base_selection.end(), is_similar);
-//          case MatchStrategy::Any:
-//            return std::any_of(m_base_selection.begin(), m_base_selection.end(), is_similar);
-//          default:
-//            Q_UNREACHABLE();
-//            return false;
-//          }
-//        }();
-//      }
-//    }
-//  }
-//  Q_EMIT scene()->mail_box().tool_appearance_changed(*this);
-//}
+void SelectSimilarTool::update_selection()
+{
+  const auto strategy = property(STRATEGY_PROPERTY_KEY)->value<MatchStrategy>();
+  for (const auto* path : scene()->item_selection<Path>()) {
+    for (auto* point : path->points()) {
+      if (!::contains(m_base_selection, point)) {
+        const auto is_similar = [point, path, this](const Point* b) {
+          return this->is_similar(*path, *point, *b);
+        };
+        const auto& begin = m_base_selection.begin();
+        const auto& end = m_base_selection.end();
+        switch (strategy) {
+        case MatchStrategy::All:
+          point->set_selected(std::all_of(begin, end, is_similar));
+          break;
+        case MatchStrategy::Any:
+          point->set_selected(std::any_of(begin, end, is_similar));
+          break;
+        default:
+          Q_UNREACHABLE();
+        }
+      }
+    }
+  }
+  Q_EMIT scene()->mail_box().tool_appearance_changed(*this);
+}
 
-//void SelectSimilarTool::update_base_selection()
-//{
-//  m_base_selection.clear();
-//  for (auto&& path : scene()->item_selection<Path>()) {
-//    for (auto it = path->begin(); it != path->end(); ++it) {
-//      if (it->is_selected) {
-//        m_base_selection.insert(it);
-//      }
-//    }
-//  }
-//}
+void SelectSimilarTool::update_base_selection()
+{
+  m_base_selection.clear();
+  for (const auto* path : scene()->item_selection<Path>()) {
+    for (auto* point : path->points()) {
+      if (point->is_selected()) {
+        m_base_selection.insert(point);
+      }
+    }
+  }
+}
 
-//bool SelectSimilarTool::is_similar(const PathIterator& a, const PathIterator& b) const
-//{
-//  using It = PathIterator;
-//  const auto alignment = property(ALIGNMENT_PROPERTY_KEY)->value<Alignment>();
-//  LINFO << (int)alignment << " " << (int)Alignment::Global << " " << (int)Alignment::Local;
-//  const std::map<Mode, std::function<double(const It&, const It)>> mode_map{
-//      {Mode::Normal, normal_distance},
-//      {Mode::X,
-//       [alignment](auto&& a, auto&& b) { return std::abs(::distance(a, b, alignment).x); }},
-//      {Mode::Y,
-//       [alignment](auto&& a, auto&& b) { return std::abs(::distance(a, b, alignment).y); }},
-//      {
-//          Mode::Distance,
-//          [alignment](auto&& a, auto&& b) { return ::distance(a, b, alignment).euclidean_norm(); },
-//      },
-//  };
+bool SelectSimilarTool::is_similar(const Path& path, const Point& a, const Point& b) const
+{
+  const auto alignment = property(ALIGNMENT_PROPERTY_KEY)->value<Alignment>();
+  const auto mode = property(MODE_PROPERTY_KEY)->value<Mode>();
+  const auto distance = [mode, &path, alignment](const Point& a, const Point& b) {
+    switch (mode) {
+    case Mode::Normal:
+      return normal_distance(a, b);
+    case Mode::X:
+      return std::abs(::distance(path, a, b, alignment).x);
+    case Mode::Y:
+      return std::abs(::distance(path, a, b, alignment).y);
+    case Mode::Distance:
+      return ::distance(path, a, b, alignment).euclidean_norm();
+    default:
+      Q_UNREACHABLE();
+      return 0.0;
+    }
+  };
 
-//  const auto mode = property(MODE_PROPERTY_KEY)->value<Mode>();
-//  const auto threshold = property(THRESHOLD_PROPERTY_KEY)->value<double>();
-//  return std::abs(mode_map.at(mode)(a, b)) < threshold;
-//}
+  const auto threshold = property(THRESHOLD_PROPERTY_KEY)->value<double>();
+  return std::abs(distance(a, b)) < threshold;
+}
 
-//void SelectSimilarTool::end()
-//{
-//  Q_EMIT scene()->mail_box().point_selection_changed();
-//}
+void SelectSimilarTool::end()
+{
+  Q_EMIT scene()->mail_box().point_selection_changed();
+}
 
-//void SelectSimilarTool::start()
-//{
-//  update_selection();
-//}
+void SelectSimilarTool::start()
+{
+  update_selection();
+}
 
-//void SelectSimilarTool::update_property_appearance()
-//{
-//  const auto mode = property(MODE_PROPERTY_KEY)->value<Mode>();
-//  auto* const threshold_property = dynamic_cast<FloatProperty*>(property(THRESHOLD_PROPERTY_KEY));
-//  static const std::map<Mode, std::tuple<QString, double>> threshold_config{
-//      {Mode::Normal, {"°", 180.0}},  // NOLINT(cppcoreguidelines-avoid-magic-numbers)
-//      {Mode::X, {"", std::numeric_limits<double>::max()}},
-//      {Mode::Y, {"", std::numeric_limits<double>::max()}},
-//      {Mode::Distance, {"", std::numeric_limits<double>::max()}},
-//  };
-//  const auto& [suffix, upper_limit] = threshold_config.at(mode);
-//  threshold_property->set_suffix(suffix);
-//  threshold_property->set_range(0.0, upper_limit);
-//}
+void SelectSimilarTool::update_property_appearance()
+{
+  const auto mode = property(MODE_PROPERTY_KEY)->value<Mode>();
+  auto* const threshold_property = dynamic_cast<FloatProperty*>(property(THRESHOLD_PROPERTY_KEY));
+  static const std::map<Mode, std::tuple<QString, double>> threshold_config{
+      {Mode::Normal, {"°", 180.0}},  // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+      {Mode::X, {"", std::numeric_limits<double>::max()}},
+      {Mode::Y, {"", std::numeric_limits<double>::max()}},
+      {Mode::Distance, {"", std::numeric_limits<double>::max()}},
+  };
+  const auto& [suffix, upper_limit] = threshold_config.at(mode);
+  threshold_property->set_suffix(suffix);
+  threshold_property->set_range(0.0, upper_limit);
+}
 
-//}  // namespace omm
+}  // namespace omm
--- src/tools/selectsimilartool.h
+++ src/tools/selectsimilartool.h
@@ -1,35 +1,35 @@
-//#pragma once
+#pragma once
 
-//#include "tools/selectpointstool.h"
+#include "tools/selectpointstool.h"
 
-//namespace omm
-//{
-//class SelectSimilarTool : public SelectPointsBaseTool
+namespace omm
+{
+class SelectSimilarTool : public SelectPointsBaseTool
 
-//{
-//public:
-//  explicit SelectSimilarTool(Scene& scene);
-//  static constexpr auto TYPE = QT_TRANSLATE_NOOP("any-context", "SelectSimilarTool");
-//  QString type() const override
-//  {
-//    return TYPE;
-//  }
-//  SceneMode scene_mode() const override
-//  {
-//    return SceneMode::Vertex;
-//  }
-//  void reset() override;
+{
+public:
+  explicit SelectSimilarTool(Scene& scene);
+  static constexpr auto TYPE = QT_TRANSLATE_NOOP("any-context", "SelectSimilarTool");
+  QString type() const override
+  {
+    return TYPE;
+  }
+  SceneMode scene_mode() const override
+  {
+    return SceneMode::Vertex;
+  }
+  void reset() override;
 
-//private:
-//  void on_property_value_changed(Property* property) override;
-//  void update_selection();
-//  void apply();
-//  void update_base_selection();
-////  bool is_similar(const PathIterator& a, const PathIterator& b) const;
-////  std::set<PathIterator> m_base_selection;
-//  void end() override;
-//  void start() override;
-//  void update_property_appearance();
-//};
+private:
+  void on_property_value_changed(Property* property) override;
+  void update_selection();
+  void apply();
+  void update_base_selection();
+  bool is_similar(const Path& path, const Point& a, const Point& b) const;
+  std::set<Point*> m_base_selection;
+  void end() override;
+  void start() override;
+  void update_property_appearance();
+};
 
-//}  // namespace omm
+}  // namespace omm
