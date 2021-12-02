a3d32571090182a3af9b78a22dbd9829ec736f6f
Pascal Bies <pascal.bies@k-lens.de>
2021-12-02 22:56:07 +0100

    implement some annotations in PathVector

--- src/path/pathvector.h
+++ src/path/pathvector.h
@@ -22,10 +22,11 @@ public:
   explicit PathVector(PathObject& path_object, DisjointPathPointSetForest& shared_joined_points);
   explicit PathVector();
   explicit PathVector(const PathVector& other, PathObject* path_object);
+  PathVector(const PathVector&) = delete;
   PathVector(PathVector&&) = delete;
   PathVector& operator=(const PathVector& other) = delete;
   PathVector& operator=(PathVector&&) = delete;
-  ~PathVector();
+  ~PathVector() override;
   static constexpr auto SEGMENTS_POINTER = "segments";
   void serialize(AbstractSerializer& serializer, const Pointer& root) const override;
   void deserialize(AbstractDeserializer& deserializer, const Pointer& root) override;
@@ -36,15 +37,15 @@ public:
   [[nodiscard]] const QPainterPath& outline() const;
   [[nodiscard]] const QPainterPath& fill() const;
   [[nodiscard]] JoinedPointIndices joined_point_indices() const;
-  std::size_t point_count() const;
-  std::deque<Path*> paths() const;
-  Path* find_path(const PathPoint& point) const;
+  [[nodiscard]] std::size_t point_count() const;
+  [[nodiscard]] std::deque<Path*> paths() const;
+  [[nodiscard]] Path* find_path(const PathPoint& point) const;
   Path& add_path(std::unique_ptr<Path>&& segment);
   std::unique_ptr<Path> remove_path(const Path& segment);
-  std::deque<PathPoint*> points() const;
-  std::deque<PathPoint*> selected_points() const;
+  [[nodiscard]] std::deque<PathPoint*> points() const;
+  [[nodiscard]] std::deque<PathPoint*> selected_points() const;
   void deselect_all_points() const;
-  PathObject* path_object() const;
+  [[nodiscard]] PathObject* path_object() const;
   [[nodiscard]] DisjointPathPointSetForest& joined_points() const;
   void update_joined_points_geometry() const;
 
