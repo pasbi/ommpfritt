1ee6d9f73b128d72437f7b5676821816f4c2be3c
Pascal Bies <pascal.bies@k-lens.de>
2022-02-09 11:45:21 +0100

    support move-idioms in transform.h

--- src/transform.h
+++ src/transform.h
@@ -30,106 +30,126 @@ template<has_insert C, typename T> void push_back(C& cs, T&& val)
   cs.insert(std::forward<T>(val));
 }
 
-// Base overload for non-associative containers
+// Base implementation for non-associative containers
 template<typename ValueType,
          template<typename...> typename ReturnContainer,
-         typename... ContainerArguments,
-         template<typename...> typename Container,
+         typename Container,
          typename F>
-auto transform(const Container<ValueType, ContainerArguments...>& cs, const F& f = F{})
+auto transform_helper(Container&& cs, const F& f = F{})
 {
-  using ReturnValueType = decltype(f(std::declval<ValueType>()));
+  using ReturnValueType = std::decay_t<decltype(f(std::declval<ValueType>()))>;
   ReturnContainer<ReturnValueType> transformed;
   if constexpr (has_reserve<ReturnContainer<ReturnValueType>>) {
     transformed.reserve(cs.size());
   }
-  for (const auto& c : cs) {
-    push_back(transformed, f(c));
+  for (auto&& c : cs) {
+    push_back(transformed, f(std::move(c)));
   }
   return transformed;
 }
 
-// Base overload for associative containers
+// Base implementation for associative containers
 template<typename ValueType,
          template<typename...> typename ReturnContainer,
          typename... ContainerArguments,
          template<typename...> typename Container,
-         typename F>
-auto transform(const Container<std::decay_t<typename ValueType::first_type>, std::decay_t<typename ValueType::second_type>, ContainerArguments...>& cs, const F& f = F{})
+         typename F,
+         typename ConcreteContainer = Container<std::decay_t<typename ValueType::first_type>,
+                                                std::decay_t<typename ValueType::second_type>,
+                                                ContainerArguments...>>
+auto transform_helper(const ConcreteContainer& cs, const F& f = F{})
 {
-  using ReturnValueType = decltype(f(std::declval<ValueType>()));
-  ReturnContainer<ReturnValueType> transformed;
-  if constexpr (has_reserve<ReturnContainer<ReturnValueType>>) {
-    transformed.reserve(cs.size());
-  }
-  for (const auto& c : cs) {
-    push_back(transformed, f(c));
-  }
-  return transformed;
+  return transform_helper<ValueType, ReturnContainer, ConcreteContainer, F>(cs, f);
 }
 
-// Base overload for array
+// Base implementation for associative containers that need to be moved
+template<typename ValueType,
+         template<typename...> typename ReturnContainer,
+         typename... ContainerArguments,
+         template<typename...> typename Container,
+         typename F,
+         typename ConcreteContainer = Container<std::decay_t<typename ValueType::first_type>,
+                                                std::decay_t<typename ValueType::second_type>,
+                                                ContainerArguments...>>
+auto transform_helper(ConcreteContainer&& cs, const F& f = F{})
+{
+  return transform_helper<ValueType, ReturnContainer, ConcreteContainer, F>(std::forward<decltype(cs)>(cs), f);
+}
+
+// overload for arrays
 template<typename ValueType,
          template<typename...> typename ReturnContainer,
          template<typename, std::size_t> typename Container,
          typename F, std::size_t N>
 auto transform(const Container<ValueType, N>& cs, const F& f = F{})
 {
-  using ReturnValueType = decltype(f(std::declval<ValueType>()));
-  ReturnContainer<ReturnValueType> transformed;
-  if constexpr (has_reserve<ReturnContainer<ReturnValueType>>) {
-    transformed.reserve(cs.size());
-  }
-  for (const auto& c : cs) {
-    push_back(transformed, f(c));
-  }
-  return transformed;
+  return transform_helper<ValueType, ReturnContainer, Container<ValueType, N>, F>(cs, f);
+}
+
+// overload for arrays that need to be moved
+template<typename ValueType,
+         template<typename...> typename ReturnContainer,
+         template<typename, std::size_t> typename Container,
+         typename F, std::size_t N>
+auto transform(Container<ValueType, N>&& cs, const F& f = F{})
+{
+  return transform_helper<ValueType, ReturnContainer, Container<ValueType, N>, F>(const_cast<Container<ValueType, N>&>(cs), f);
 }
 
 // overload for explicit return value type
-template<typename ReturnValueType,
-         template<typename...> typename Container,
-         typename... ContainerArgs>
+template<typename ReturnValueType, template<typename...> typename Container, typename... ContainerArgs>
 auto transform(const Container<ContainerArgs...>& cs)
 {
   auto f = [](auto&& v) { return static_cast<ReturnValueType>(v); };
-  return transform<typename Container<ContainerArgs...>::value_type, Container>(cs, f);
+  return transform_helper<typename Container<ContainerArgs...>::value_type, Container>(cs, f);
+}
+
+// overload for explicit return value type (move)
+template<typename ReturnValueType, template<typename...> typename Container, typename... ContainerArgs>
+auto transform(Container<ContainerArgs...>&& cs)
+{
+  auto f = [](auto&& v) { return static_cast<ReturnValueType>(v); };
+  return transform_helper<typename Container<ContainerArgs...>::value_type, Container>(std::move(cs), f);
 }
 
 // overload for explicit return container type and functor
-template<template<typename...> typename ReturnContainer,
-         typename Container,
-         typename F>
-auto transform(const Container& cs, const F& f)
+template<template<typename...> typename ReturnContainer, typename Container, typename F>
+auto transform(Container&& cs, const F& f)
 {
-  return transform<typename Container::value_type, ReturnContainer>(cs, f);
+  using value_type = typename std::decay_t<Container>::value_type;
+  return transform_helper<value_type, ReturnContainer>(std::forward<Container>(cs), f);
 }
 
 // overload for explicit return container type
 template<template<typename...> typename ReturnContainer, typename Container>
-auto transform(const Container& cs)
+auto transform(Container&& cs)
 {
   constexpr auto f = [](auto&& v) { return v; };
-  return transform<typename Container::value_type, ReturnContainer>(cs, f);
+  using value_type = typename std::decay_t<Container>::value_type;
+  return transform_helper<value_type, ReturnContainer>(std::forward<Container>(cs), f);
 }
 
 // overload for explicit return value type and return container type
-template<typename ReturnValueType,
-         template<typename ...> typename ReturnContainer,
-         typename Container>
-auto transform(const Container& cs)
+template<typename ReturnValueType, template<typename ...> typename ReturnContainer, typename Container>
+auto transform(Container&& cs)
 {
   constexpr auto f = [](auto&& v) { return static_cast<ReturnValueType>(v); };
-  return transform<typename Container::value_type, ReturnContainer>(cs, f);
+  using value_type = typename std::decay_t<Container>::value_type;
+  return transform_helper<value_type, ReturnContainer>(std::forward<Container>(cs), f);
 }
 
 // overload for explicit return container type and functor
-template<template<typename...> typename Container,
-         typename... ContainerArgs,
-         typename F>
+template<template<typename...> typename Container, typename... ContainerArgs, typename F>
 auto transform(const Container<ContainerArgs...>& cs, const F& f)
 {
-  return transform<typename Container<ContainerArgs...>::value_type, Container>(cs, f);
+  return transform_helper<typename Container<ContainerArgs...>::value_type, Container>(cs, f);
+}
+
+// overload for explicit return container type and functor (move)
+template<template<typename...> typename Container, typename... ContainerArgs, typename F>
+auto transform(Container<ContainerArgs...>&& cs, const F& f)
+{
+  return transform_helper<typename Container<ContainerArgs...>::value_type, Container>(std::move(cs), f);
 }
 
 }  // namespace omm::util
--- test/unit/transform.cpp
+++ test/unit/transform.cpp
@@ -2,6 +2,7 @@
 #include "gtest/gtest.h"
 #include <set>
 #include <vector>
+#include <deque>
 
 TEST(TransformTest, explicit_value_type)
 {
@@ -9,6 +10,14 @@ TEST(TransformTest, explicit_value_type)
   const auto result = util::transform<double>(set);
   static_assert(std::is_same_v<decltype(result)::value_type, double>);
   EXPECT_EQ(result, (std::set{1.0, 2.0, 3.0, 4.0}));
+}
+
+TEST(TransformTest, explicit_container_and_functor)
+{
+  std::set set{1, 2, 3, 4};
+  const auto result = util::transform<std::vector>(set, [](int i) { return i / 2.0; });
+  static_assert(std::is_same_v<decltype(result)::value_type, double>);
+  EXPECT_EQ(result, (std::vector{0.5, 1.0, 1.5, 2.0}));
 }
 
 TEST(TransformTest, explicit_container_type)
@@ -25,14 +34,6 @@ TEST(TransformTest, explicit_container_and_value_type)
   const auto result = util::transform<double, std::vector>(set);
   static_assert(std::is_same_v<decltype(result)::value_type, double>);
   EXPECT_EQ(result, (std::vector{1.0, 2.0, 3.0, 4.0}));
-}
-
-TEST(TransformTest, explicit_container_and_functor)
-{
-  std::set set{1, 2, 3, 4};
-  const auto result = util::transform<std::vector>(set, [](int i) { return i / 2.0; });
-  static_assert(std::is_same_v<decltype(result)::value_type, double>);
-  EXPECT_EQ(result, (std::vector{0.5, 1.0, 1.5, 2.0}));
 }
 
 TEST(TransformTest, explicit_functor)
@@ -56,8 +57,40 @@ TEST(TransformTest, map)
 
 TEST(TransformTest, array)
 {
-//  std::array array{1, 2, 3, 4};
-//  const auto result = util::transform<std::vector>(array, [](int i) { return i / 2.0; });
-//  static_assert(std::is_same_v<decltype(result)::value_type, double>);
-//  EXPECT_EQ(result, (std::vector{0.5, 1.0, 1.5, 2.0}));
+  std::array array{1, 2, 3, 4};
+  const auto result = util::transform<std::vector>(array, [](int i) { return i / 2.0; });
+  static_assert(std::is_same_v<decltype(result)::value_type, double>);
+  EXPECT_EQ(result, (std::vector{0.5, 1.0, 1.5, 2.0}));
+}
+
+TEST(TransformTest, move)
+{
+  struct NoCopy
+  {
+    explicit NoCopy(int payload) : payload(payload) {}
+    NoCopy(const NoCopy&) = delete;
+    NoCopy(NoCopy&&) = default;
+    NoCopy &operator=(const NoCopy&) = delete;
+    NoCopy &operator=(NoCopy&&) = default;
+    int payload;
+    bool operator==(const NoCopy& other)
+    {
+      return payload == other.payload;
+    }
+  };
+
+  std::vector<NoCopy> vector;
+  vector.emplace_back(1);
+  vector.emplace_back(2);
+  vector.emplace_back(3);
+  vector.emplace_back(4);
+  const auto f = [](NoCopy&& v) -> decltype(auto) { return std::move(v); };
+  auto deque = util::transform<std::deque>(std::move(vector), f);
+
+  std::deque<NoCopy> expected_deque;
+  expected_deque.emplace_back(1);
+  expected_deque.emplace_back(2);
+  expected_deque.emplace_back(3);
+  expected_deque.emplace_back(4);
+  EXPECT_EQ(deque, expected_deque);
 }
