c8b3f88cb9891da2bc7ea50264f6eda8947b8c2c
Pascal Bies <pascal.bies@k-lens.de>
2022-02-10 10:34:10 +0100

    remove empty files

--- src/nodesystem/common.cpp
+++
@@ -1,9 +0,0 @@
-#include "nodesystem/common.h"
-#include "properties/property.h"
-#include "properties/stringproperty.h"
-#include "properties/triggerproperty.h"
-
-namespace omm::nodes
-{
-
-}  // namespace omm::nodes
