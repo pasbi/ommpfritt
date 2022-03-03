39e8c6743e1e02488551384d4bc19fb75cbcc497
pasbi <pasbi@users.noreply.github.com>
2022-03-03 17:46:09 +0100

    Simplify linux build script
    No more arguments:
    - source directory is derived from the script location
    - config is hardcoded to `Release`. If you need a debug or profiling, you should know how to change that
    - usage of ninja enables parallel builds by default.

--- build-scripts/build-omm-with-icons.sh
+++ build-scripts/build-omm-with-icons.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-usage="Usage: $0 SOURCE CONFIG [CMAKE_ARGUMENTS ...]
-This script builds ommpfritt with icons from SOURCE (root of this repository).
-CONFIG can be 'Release' or 'Debug'.
+usage="Usage: $0 [CMAKE_ARGUMENTS ...]
+This script builds ommpfritt with icons from source (root of this repository).
 Builds and installs the application into '/tmp', see output for details.
 Additional CMAKE_ARGUMENTS are passed verbatim to the cmake configuration call.
 "
@@ -10,28 +9,22 @@ if [[ "$1" =~ (-h|--help) ]]; then
     echo "$usage"
     exit 0
 fi
-if [ ! "$2" ]; then
-    1>&2 echo "$usage"
-    exit 1
-fi
-
 set -e
+set -x
 
-source=$1
-config=$2
+source=$(dirname $0)/..
+config=Release
 
 build=$(mktemp -d -t build-omm-XXXX)
 install=$build/install
-ncores=4
 
 echo "Building omm "
 echo "  source:  $source"
 echo "  build:   $build"
 echo "  config:  $config"
 echo "  install: $install"
-echo "  ncores:  $ncores"
 
-cmake -S "$source" -B "$build" -DCMAKE_INSTALL_PREFIX="$install" -DCMAKE_BUILD_TYPE="$config" "$@"
+cmake -S "$source" -B "$build" -GNinja -DCMAKE_INSTALL_PREFIX="$install" -DCMAKE_BUILD_TYPE="$config" "$@"
 cmake --build "$build" --target install --config "$config" -j$ncores
 cmake --build "$build" --target icons -j$ncores
 cmake -S "$source" -B "$build"
