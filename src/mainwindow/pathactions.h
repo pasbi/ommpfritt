e6af99a16fe2e66a42d3cde26b2acb648bbe7839
Pascal Bies <pascal.bies@k-lens.de>
2022-03-20 22:43:02 +0100

    split large function `convert_objects_recursively`

--- src/mainwindow/pathactions.h
+++ src/mainwindow/pathactions.h
@@ -5,8 +5,10 @@
 
 namespace omm
 {
+
 class Scene;
 class Application;
+class Object;
 
 }  // namespace omm
 
@@ -14,6 +16,7 @@ namespace omm::path_actions
 {
 
 std::set<QString> available_actions();
+std::set<Object*> convert_objects(Application& app);
 bool perform_action(Application& app, const QString& name);
 
 }  // namespace omm::path_actions
